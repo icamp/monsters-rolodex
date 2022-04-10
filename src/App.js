b3cecad0ad4d6fd07cfee8de2b3b4b980fefd836
icamp <campan.ionel@gmail.com>
2022-04-10 10:43:31 +0300

    keys for mapping

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ class App extends Component {
       monsters: [
         {
           name: "Linda",
+          id: "dsflgj9",
         },
         {
           name: "Frank",
+          id: "r29hr923r",
         },
         {
           name: "Jacky",
+          id: "fwek9th",
         },
         {
           name: "John",
+          id: "fmnw4gh39h",
         },
       ],
     };
@@ -27,7 +31,11 @@ class App extends Component {
     return (
       <div className='App'>
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
