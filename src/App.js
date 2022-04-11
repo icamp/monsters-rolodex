3ab1e7f7bbc826b2955c6219419ceb73a9cbe701
icamp <campan.ionel@gmail.com>
2022-04-11 18:14:35 +0300

    lifecycle method: componentDidMount

--- src/App.js
+++ src/App.js
@@ -6,25 +6,23 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-          id: "dsflgj9",
-        },
-        {
-          name: "Frank",
-          id: "r29hr923r",
-        },
-        {
-          name: "Jacky",
-          id: "fwek9th",
-        },
-        {
-          name: "John",
-          id: "fmnw4gh39h",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
